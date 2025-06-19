7bc39117f5a8d5dca7cb1086845d816796d3b68f
Arkomustafi <64455147+Arkomustafi@users.noreply.github.com>
2025-06-19 22:42:43 +0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import './index.css'; // Corrected from './Index.css' to './index.css'
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
